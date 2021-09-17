2a36844c54b3e6ef6cbb1eef0b35cf147d7ebdf9
Javi Plaza <javierplazasisques@gmail.com>
2021-09-17 11:24:38 +0200

    Home view designed

---
+++ Project-Killer-Whale/Controller/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Project-Killer-Whale
+//
+//  Created by Javier Plaza Sisqués on 17/9/21.
+//
+
+import UIKit
+import MapKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
